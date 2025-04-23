47200b80960c68e69122fd8a60bc2b516d07b5c4
flashhhhh <nguyenchithanhpro196@gmail.com>
2025-04-23 21:42:10 +0700

    fix: change Kafka Consumer to Kafka Consumer Group

--- kafka/consumer.go
+++ kafka/consumer.go
@@ -1,36 +1,67 @@
 package kafka
 
 import (
+	"context"
+	"log"
+	"os"
+	"os/signal"
+
 	"github.com/IBM/sarama"
 )
 
-type KafkaConsumer struct {
-	consumer sarama.Consumer
+type KafkaConsumerGroup struct {
+	consumerGroup sarama.ConsumerGroup
+	topics        []string
+	ctx           context.Context
+	cancel        context.CancelFunc
 }
 
-func NewKafkaConsumer(brokers []string) (*KafkaConsumer, error) {
-	consumer, err := sarama.NewConsumer(brokers, nil)
+// NewKafkaConsumerGroup creates a new KafkaConsumerGroup instance
+func NewKafkaConsumerGroup(brokers []string, groupID string, topics []string) (*KafkaConsumerGroup, error) {
+	config := sarama.NewConfig()
+	config.Version = sarama.V2_1_0_0
+	config.Consumer.Group.Rebalance.Strategy = sarama.NewBalanceStrategyRoundRobin()
+	config.Consumer.Offsets.Initial = sarama.OffsetOldest
+
+	cg, err := sarama.NewConsumerGroup(brokers, groupID, config)
 	if err != nil {
 		return nil, err
 	}
 
-	return &KafkaConsumer{
-		consumer: consumer,
+	ctx, cancel := context.WithCancel(context.Background())
+
+	return &KafkaConsumerGroup{
+		consumerGroup: cg,
+		topics:        topics,
+		ctx:           ctx,
+		cancel:        cancel,
 	}, nil
 }
 
-func (kc *KafkaConsumer) ConsumePartition(topic string, partition int32, offset int64) (sarama.PartitionConsumer, error) {
-	partitionConsumer, err := kc.consumer.ConsumePartition(topic, partition, offset)
-	if err != nil {
-		return nil, err
-	}
-	return partitionConsumer, nil
+// StartConsuming starts the consumer loop using the given handler
+func (k *KafkaConsumerGroup) StartConsuming(handler sarama.ConsumerGroupHandler) {
+	go func() {
+		for {
+			err := k.consumerGroup.Consume(k.ctx, k.topics, handler)
+			if err != nil {
+				log.Printf("Error from consumer: %v", err)
+			}
+			if k.ctx.Err() != nil {
+				return
+			}
+		}
+	}()
+
+	go func() {
+		sigterm := make(chan os.Signal, 1)
+		signal.Notify(sigterm, os.Interrupt)
+		<-sigterm
+		k.Stop()
+	}()
 }
 
-func (kc *KafkaConsumer) Close() error {
-	err := kc.consumer.Close()
-	if err != nil {
-		return err
-	}
-	return nil
+// Stop gracefully shuts down the consumer group
+func (k *KafkaConsumerGroup) Stop() {
+	k.cancel()
+	_ = k.consumerGroup.Close()
 }
